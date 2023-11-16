f77ede2e5b36614ff9e932445046d90229cc7cac
Robert W. Pink <robert.pink@oneh2.com>
2023-11-16 13:35:21 -0500

    .

--- SerialCLI.ino
+++ SerialCLI.ino
@@ -186,8 +186,6 @@ void printOuts(unsigned long inter){
       Serial.print(numDisp);
       Serial.print(", ");
       for(int i = 0; i < numDisp; i++){
-        Serial.print(i+1); //dispenser #
-        Serial.print(",");
         Serial.print(dispensers[i].currentPsi);
         Serial.print(",");
         Serial.print(dispensers[i].targetPsi);
